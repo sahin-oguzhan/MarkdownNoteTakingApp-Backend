4533abcf90d00ffd5f10b395b47dd50327c885da
sahin-oguzhan <oguzhan.sahin99@outlook.com>
2025-11-13 01:01:01 +0300

    Initialize Spring Boot project with Maven wrapper
    Added initial Spring Boot application structure, Maven wrapper scripts, and configuration files. Includes basic application and test classes, .gitignore, and pom.xml with dependencies for JPA, Security, Validation, WebMVC, PostgreSQL, Lombok, and JWT support.

---
+++ mntapp/src/main/java/com/oguzhan/mntapp/MntappApplication.java
@@ -0,0 +1,13 @@
+package com.oguzhan.mntapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MntappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MntappApplication.class, args);
+	}
+
+}
---
+++ mntapp/src/test/java/com/oguzhan/mntapp/MntappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oguzhan.mntapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MntappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
